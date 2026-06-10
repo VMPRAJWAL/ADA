0e6642818314b53fa00607fd027f45ea108971d6
PRAJWAL V <dynamiteactiva@gmail.com>
2026-06-10 07:39:17 +0530

    Refactor knapsack algorithm and improve structure
    Refactored knapsack implementation to improve clarity and structure. Added separate knapsack function and adjusted variable names for consistency.

--- 5knapsack.c
+++ 5knapsack.c
@@ -1,55 +1,57 @@
 #include<stdio.h>
 
-int max(int a, int b){ return a>b ? a : b;}
+int max(int a, int b){ return (a>b) ? a : b;}
 
-int main(){
-    int n;
-    printf("Enter n value: ");
-    scanf("%d", &n);
-    int wt[n+1], p[n+1], w, i, cap;
+void knapsack(int *wt, int *profit, int n, int cap){
+    int dp[n+1][cap+1];
 
-    printf("Enter the weights: \n");
-    for(i=1; i<=n; i++){
-        scanf("%d", &wt[i]);
-    }
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=cap; j++){
+            if(i==0 || j==0) dp[i][j] = 0;
 
-    printf("Enter the profits: \n");
-    for(i=1; i<=n; i++){
-        scanf("%d", &p[i]);
-    }
-    
-    printf("Enter the capacity: ");
-    scanf("%d", &cap);
-
-    int dp[n+1][cap+1]; 
-    
-    for(i=0; i<=n; i++){
-        for(w=0; w<=cap; w++){
-            if(i==0 || w==0) dp[i][w]=0;
-            
-            else if(wt[i] <= w){
-                int a= p[i] + dp[i-1][w-wt[i]];
-                int b= dp[i-1][w];
-                dp[i][w] = max(a, b);
+            else if(wt[i]<=j){
+                int a= profit[i] + dp[i-1][j-wt[i]];
+                int b= dp[i-1][j];
+                dp[i][j] = max(a,b);
             }
 
-            else dp[i][w] = dp[i-1][w];
-        }
-    }
-
-    printf("Items Included: ");
-    printf("Items\tWeight\tProfit\n");
-
-    w=cap;
-    int tp= dp[n][cap];
-    for(i=n; i>0 && tp>0; i--){
-        if(tp!= dp[i-1][w]){
-            printf("%d\t%d\t%d\n", i, wt[i], p[i]);
-            tp -= p[i];
-            w -= wt[i];
+            else dp[i][j] = dp[i-1][j];
         }
     }
 
     printf("Maximum profit: %d\n", dp[n][cap]);
-    return 0;
+
+    printf("Items included in the knapsack:\n");
+    int j=cap;
+    int tp= dp[n][cap];
+
+    for(int i=n; i>0 && tp>0; i--){
+        if(tp != dp[i-1][j]){
+            printf("Item %d (weight: %d, profit: %d)\n", i, wt[i], profit[i]);
+            tp-=profit[i];
+            j-=wt[i];
+        }
+    }
 }
+
+void main(){
+    int n, cap;
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+    printf("Enter the capacity of the knapsack: ");
+    scanf("%d", &cap);
+
+    int wt[n+1], profit[n+1];
+    printf("Enter the weights of the items:\n");
+    for(int i=1; i<=n; i++){
+        scanf("%d", &wt[i]);
+    }
+
+    printf("Enter the profits of the items:\n");
+    for(int i=1; i<=n; i++){
+        scanf("%d", &profit[i]);
+    }
+
+    knapsack(wt, profit, n, cap);
+}
+//Time Complexity: O(n*cap) in all cases
